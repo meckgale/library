7e2c1119e1fad7a4912a100cb612f60251044fa8
meck <meckgalen@gmail.com>
2023-06-21 03:00:50 +0300

    Delete book button and read checkbox need to be updated. Rest is functional. Footer is missing.

--- script.js
+++ script.js
@@ -1,7 +1,123 @@
+const bookForm = document.querySelector('#book-form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
+const bookShelf = document.querySelector('.book-shelf');
 const openFormBtn = document.querySelector('.header-btn');
 const closeFormBtn = document.querySelector('.book-form-close-btn');
 const formFrame = document.querySelector('.form-frame');
 
+//Book samples
+let ex1 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', '1178', false);
+let ex2 = new Book('A Game of Thrones', 'George R. R. Martin', '694', false);
+let ex3 = new Book('Harry Potter and the Half-Blood Prince', 'J. K. Rowling', '607', true);
+
+//Open form
 openFormBtn.addEventListener('click', () => formFrame.classList.add('active'));
 
+//Close form
 closeFormBtn.addEventListener('click', () => formFrame.classList.remove('active'));
+
+//Library
+const myLibrary = [ex1, ex2, ex3];
+
+//The constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+//Add books to library
+bookForm.addEventListener('submit', (e) => {
+    formFrame.classList.remove('active');
+    e.preventDefault();
+    const book = new Book(title.value, author.value, pages.value, read.checked);
+    myLibrary.push(book);
+    clearInputs();
+    render();
+});
+
+//Clear the form inputs
+function clearInputs() {
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.checked = false;
+}
+
+function render() {
+    bookShelf.textContent = '';
+    myLibrary.forEach((book, index) => {
+        createBookCard(book, index);
+    });
+}
+
+//Remove the book element
+
+
+
+
+//Create the Book elements for the page
+function createBookCard(book, index) {
+
+    //Create the card div
+    const newCardDiv = document.createElement('div');
+    newCardDiv.classList.add('card');
+
+    //Add title to the div
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList.add('card-title');
+    cardTitle.innerText = book.title;
+    newCardDiv.append(cardTitle);
+
+    //Add the author name to the div
+    const cardAuthor = document.createElement('p');
+    cardAuthor.classList.add('card-author');
+    cardAuthor.innerText = book.author;
+    newCardDiv.append(cardAuthor);
+
+    //Add the pages to the div
+    const cardPages = document.createElement('p');
+    cardPages.classList.add('card-pages');
+    cardPages.innerText = book.pages;
+    newCardDiv.append(cardPages);
+
+    //Add checkbox for read value
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('data', index);
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.classList.add('check-box');
+    checkBox.checked = book.read;
+    newCardDiv.append(checkBox);
+
+    // //Add label for checkbox
+    // const checkBoxLabel = document.createElement('label');
+    // checkBoxLabel.setAttribute('for',`checkbox${index}`);
+    // checkBoxLabel.classList.add('check-box-label');
+    // //Add icon for the label
+    // const checkBoxIcon = document.createElement('img');
+    // checkBoxIcon.setAttribute('src', './icon/read.svg');
+    // checkBoxIcon.classList.add('check-box-icon');
+    // checkBoxLabel.append(checkBoxIcon);
+    // checkBox.append(checkBoxLabel);
+
+
+    //Add remove button for book card
+    const removeBtn = document.createElement('button');
+    removeBtn.setAttribute('data', index);
+    removeBtn.classList.add('remove-btn');
+    //Add remove icon for the button
+    const removeBtnIcon = document.createElement('img');
+    removeBtnIcon.setAttribute('src', './icon/delete.svg');
+    removeBtnIcon.classList.add('remove-btn-icon');
+    removeBtn.append(removeBtnIcon);
+    newCardDiv.append(removeBtn);
+
+    //Add the new book to book section of the page
+    bookShelf.append(newCardDiv);
+}
+
+render();
